e9c21164bfea18fceb3925a7f10bd9c78e5177a7
Qabreira <cabreir@gmail.com>
2023-07-15 17:09:55 -0300

    inclusão de novos testes e criação de custom command para reaproveitamento de código

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,61 +1,105 @@
 /// <reference types="cypress" />
-
-// IMPORTAÇÃO DA MASSA DE TESTES EXTERNA USANDO O PRÓPRIO JAVASCRIPT
-import user from '../fixtures/users.json'
+import users from '../fixtures/users.json'
 
 describe('login', () => {
 
-    // GANCHO DO CYPRESS PARA IMPORTAR A MASSA DE TESTES DE FIXTURES
-    // before(function () {
-    //     cy.fixture('users.json').then(function (user) {
-    //         this.user = user
-    //     })
-    // })
+    it('deve logar com sucesso', () => {
+        const user = users.loginSuccess
 
-    it('deve logar com o perfil do admin', function () {
-        // MASSA DE TESTE DENTRO DO TESTE
-        // const user = {
-        //     name: 'Admin',
-        //     email: 'admin@healthxp.com',
-        //     password: 'xperience'
-        // }
+        // CRIAÇÃO DE CUSTOM COMMAND PARA REAPROVEITAMENTO DE CÓDIGO
+        // cy.visit('/').title()
+        //     .should('eq', 'Health eXperience | Exclusivo para treinamentos na QAx')
 
-        // MASSA DE TESTE EXTERNA IMPORTADA VIA COMANDO DO CYPRESS
-        // cy.fixture('users.json').then(function (user) {
-        //     cy.log(JSON.stringify(user))
-        // })
-        //
-        // const user = this.user
+        // cy.get('input[name=email]').should('be.visible')
+        //     .type(user.email)
+        // cy.get('input[name=password]').should('be.visible')
+        //     .type(user.password)
+
+        // cy.contains('button', 'Entrar').should('be.visible')
+        //     .click()
+
+        cy.doLogin(user.email, user.password)
+
+        cy.get('.logged-user').should('contain', `Olá, ${user.name}`)
+            .and('be.visible')
+    })
+
+    it('não deve logar com senha incorreta', () => {
+        const user = users.wrongPassword
+        cy.doLogin(user.email, user.password)
+
+        cy.contains('#swal2-content', user.alertMessage)
+            .should('be.visible')
+    })
+
+    it('não deve logar com usuário não cadastrado', () => {
+        const user = users.unknownUser
+        cy.doLogin(user.email, user.password)
+
+        cy.contains('#swal2-content', user.alertMessage)
+            .should('be.visible')
+    })
+
+    it('não deve logar com email incorreto', () => {
+        const user = users.wrongEmail
+        cy.doLogin(user.email, user.password)
+
+        cy.contains('#swal2-content', user.alertMessage)
+            .should('be.visible')
+    })
+
+    it('não deve logar com e-mail em branco', () => {
+        const user = users.emptyEmail
 
         cy.visit('/').title()
             .should('eq', 'Health eXperience | Exclusivo para treinamentos na QAx')
 
-        cy.get('input[name=email]').should('be.visible')
+        cy.get('input[name=email]')
             .type(user.email)
+            .clear()
+            .should('be.visible')
         cy.get('input[name=password]').should('be.visible')
             .type(user.password)
 
         cy.contains('button', 'Entrar').should('be.visible')
             .click()
 
-        cy.get('.logged-user').should('contain', `Olá, ${user.name}`)
-            .and('be.visible')
+        cy.contains('#swal2-content', user.alertMessage)
+            .should('be.visible')
     })
 
-    /*     it.skip('deve fazer login via api request', () => {
-            const user = {
-                email: 'admin@healthxp.com',
-                password: 'xperience'
-            }
-    
-            cy.request({
-                url: 'http://localhost:3333/sessions',
-                method: 'POST',
-                body: user,
-                failOnStatusCode: false
-            }).then(response => {
-                expect(response.status).to.eq(200)
-                cy.log(JSON.stringify(response.body.token))
-            })
-        }) */
+    it('não deve logar com a senha em branco', () => {
+        const user = users.emptyPassword
+
+        cy.visit('/').title()
+            .should('eq', 'Health eXperience | Exclusivo para treinamentos na QAx')
+
+        cy.get('input[name=email]').should('be.visible')
+            .type(user.email)
+        cy.get('input[name=password]')
+            .type(user.password)
+            .clear()
+            .should('be.visible')
+
+        cy.contains('button', 'Entrar').should('be.visible')
+            .click()
+
+        cy.contains('#swal2-content', user.alertMessage)
+            .should('be.visible')
+    })
+})
+
+// CUSTOM COMMAND CRIADO PARA REAPROVEITAR CÓDIGOS QUE SE REPETEM
+// esse comando deve ser incluso no arquivo "commands.js" da pasta "support"
+Cypress.Commands.add('doLogin', (email, password) => {
+    cy.visit('/').title()
+        .should('eq', 'Health eXperience | Exclusivo para treinamentos na QAx')
+
+    cy.get('input[name=email]').should('be.visible')
+        .type(email)
+    cy.get('input[name=password]').should('be.visible')
+        .type(password)
+
+    cy.contains('button', 'Entrar').should('be.visible')
+        .click()
 })
