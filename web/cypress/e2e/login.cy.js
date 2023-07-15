3e80d2da8926514c0692dbafa0f6c766444290dd
Qabreira <cabreir@gmail.com>
2023-07-15 17:26:44 -0300

    refatorando o código utilizando custom commands

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -5,20 +5,7 @@ describe('login', () => {
 
     it('deve logar com sucesso', () => {
         const user = users.loginSuccess
-
-        // CRIAÇÃO DE CUSTOM COMMAND PARA REAPROVEITAMENTO DE CÓDIGO
-        // cy.visit('/').title()
-        //     .should('eq', 'Health eXperience | Exclusivo para treinamentos na QAx')
-
-        // cy.get('input[name=email]').should('be.visible')
-        //     .type(user.email)
-        // cy.get('input[name=password]').should('be.visible')
-        //     .type(user.password)
-
-        // cy.contains('button', 'Entrar').should('be.visible')
-        //     .click()
-
-        cy.doLogin(user.email, user.password)
+        cy.doLogin(user)
 
         cy.get('.logged-user').should('contain', `Olá, ${user.name}`)
             .and('be.visible')
@@ -26,26 +13,23 @@ describe('login', () => {
 
     it('não deve logar com senha incorreta', () => {
         const user = users.wrongPassword
-        cy.doLogin(user.email, user.password)
+        cy.doLogin(user)
 
-        cy.contains('#swal2-content', user.alertMessage)
-            .should('be.visible')
+        cy.checkAlertMessage(user.alertMessage)
     })
 
     it('não deve logar com usuário não cadastrado', () => {
         const user = users.unknownUser
-        cy.doLogin(user.email, user.password)
+        cy.doLogin(user)
 
-        cy.contains('#swal2-content', user.alertMessage)
-            .should('be.visible')
+        cy.checkAlertMessage(user.alertMessage)
     })
 
     it('não deve logar com email incorreto', () => {
         const user = users.wrongEmail
-        cy.doLogin(user.email, user.password)
+        cy.doLogin(user)
 
-        cy.contains('#swal2-content', user.alertMessage)
-            .should('be.visible')
+        cy.checkAlertMessage(user.alertMessage)
     })
 
     it('não deve logar com e-mail em branco', () => {
@@ -64,8 +48,7 @@ describe('login', () => {
         cy.contains('button', 'Entrar').should('be.visible')
             .click()
 
-        cy.contains('#swal2-content', user.alertMessage)
-            .should('be.visible')
+        cy.checkAlertMessage(user.alertMessage)
     })
 
     it('não deve logar com a senha em branco', () => {
@@ -84,22 +67,6 @@ describe('login', () => {
         cy.contains('button', 'Entrar').should('be.visible')
             .click()
 
-        cy.contains('#swal2-content', user.alertMessage)
-            .should('be.visible')
+        cy.checkAlertMessage(user.alertMessage)
     })
-})
-
-// CUSTOM COMMAND CRIADO PARA REAPROVEITAR CÓDIGOS QUE SE REPETEM
-// esse comando deve ser incluso no arquivo "commands.js" da pasta "support"
-Cypress.Commands.add('doLogin', (email, password) => {
-    cy.visit('/').title()
-        .should('eq', 'Health eXperience | Exclusivo para treinamentos na QAx')
-
-    cy.get('input[name=email]').should('be.visible')
-        .type(email)
-    cy.get('input[name=password]').should('be.visible')
-        .type(password)
-
-    cy.contains('button', 'Entrar').should('be.visible')
-        .click()
 })
--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -10,9 +10,26 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
+// CUSTOM COMMAND PARA REALIZAR OS PASSOS DE LOGON NA APLICAÇÃO
+Cypress.Commands.add('doLogin', (user) => {
+    cy.visit('/').title()
+        .should('eq', 'Health eXperience | Exclusivo para treinamentos na QAx')
+
+    cy.get('input[name=email]').should('be.visible')
+        .type(user.email)
+    cy.get('input[name=password]').should('be.visible')
+        .type(user.password)
+
+    cy.contains('button', 'Entrar').should('be.visible')
+        .click()
+})
+
+// CUSTOM COMMAND PARA REALIZAR A VERIFICAÇÃO DA MENSAGEM DE ALERTA
+Cypress.Commands.add('checkAlertMessage', (alertMessage) => {
+    cy.contains('#swal2-content', alertMessage)
+        .should('be.visible')
+})
+
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 //
