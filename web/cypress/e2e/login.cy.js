2307ec5efca49e1e8cc9248e411d4f985937a3c8
Qabreira <cabreir@gmail.com>
2023-07-19 21:17:45 -0300

    refatorando teste de e-mail inválido para fazer uma asserção única da massa de teste em lista

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -23,16 +23,35 @@ describe('login', () => {
         login.checkAlertMessage(user.alertMessage)
     })
 
-    it('não deve logar com email incorreto', () => {
+    it.only('não deve logar com email incorreto', () => {
         const emails = users.wrongEmail.emails
         const alertMessage = users.wrongEmail.alertMessage
+        let outputMessages = []
+        let expectedMessages = []
+
         login.go()
+
+        // EXECUTAR O PROCESSO COM TODOS OS ITENS DE UMA MASSA DE TESTES EM LISTA
         emails.forEach((i) => {
             login.fillCredentials(i)
             login.submit()
-            login.checkAlertMessage(alertMessage)
+
+            // PEGAR O TEXTO DO POPUP QUE EXIBE MENSAGENS DE ALERTA
+            cy.get('#swal2-content')
+                .invoke('text')
+                .then((text) => {
+                    // ENVIAR O TEXTO PARA UMA VARIÁVEL DO TIPO LISTA
+                    cy.log(text)
+                    outputMessages.push(text)
+                    // ALIMENTAR OUTRA LISTA COM A MENSAGEM ESPERADA
+                    expectedMessages.push(alertMessage)
+                })
+
+            // login.checkAlertMessage(alertMessage)
             login.returnCredentials()
         })
+        // COMPARAR AS LISTAS CRIADAS VERIFICANDO SE SÃO IGUAIS
+        cy.wrap(outputMessages).should('deep.equal', expectedMessages)
     })
 
     it('não deve logar com e-mail em branco', () => {
