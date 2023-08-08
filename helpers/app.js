88c27e4dd945f69a366219b7853d122246cf0a0a
Qabreira <cabreir@gmail.com>
2023-08-07 22:39:07 -0300

    incluindo manipulação de massa de testes no bd por meio de acionamento de api helper

--- helpers/app.js
+++ helpers/app.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const app = express()
 
-// INFORMAR QUE O FORMATO UTILIZADO SERÁ JSON
 app.use(express.json())
 
-// CRIAÇÃO DE UMA ROTA PARA RETONAR UMA MENSAGEM
+const db = require('./db')
+
 app.get('/', function (req, res) {
     res.json({ message: 'Hello API Helper' })
 })
 
-// CRIAÇÃO DE UMA ROTA PARA RECEBER UM CORPO JSON E RETORNAR NA RESPOSTA
-app.post('/students', function (req, res) {
-    const student = req.body
-    res.json(student)
-})
+// REQUISITANDO ESTA ROTA UM ESTUDANTE É DELETADO (CASO EXISTIR)
+// E RECRIADO PARA PREPARAR A PRÉ CONDIÇÃO DE TESTE :)
+app.post('/students', db.deleteAndCreateStudent)
 
 app.listen(5000)
---
+++ helpers/db.js
@@ -0,0 +1,37 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    host: 'babar.db.elephantsql.com',
+    user: 'qrucqdqk',
+    password: 'haF3RmWvkhNavnI6TQ_6ADUmnZ3phWtv',
+    database: 'qrucqdqk',
+    port: 5432
+})
+
+const deleteAndCreateStudent = (req, res) => {
+
+    const student = req.body
+
+    const query = `
+    WITH add AS (
+      INSERT INTO students (name, email, age, weight, feet_tall)
+      VALUES ($1, $2, $3, $4, $5)
+    )
+    DELETE FROM students WHERE email = $2;
+    `
+    const values = [
+        student.name, student.email, student.age, student.weight, student.feet_tall
+    ]
+
+    pool.query(query, values, function (error, result) {
+        if (error) {
+            // COM O RETURN, CASO OCORRER UM ERRO A EXECUÇÃO É "ABORTADA" NESTA LINHA
+            return res.status(500).json(error)
+        }
+        res.status(201).json(result)
+    })
+}
+
+module.exports = {
+    deleteAndCreateStudent
+}
